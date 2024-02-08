3cb2408daead90e99f96a4c9cd45493a70aef543
iamefe <efeativie@gmail.com>
2024-02-08 21:41:39 +0100

    added icons

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 slint::include_modules!();
-use slint::SharedString;
 
 const TAXPER: f64 = 0.3;
 const OWNERPER: f64 = 0.55;
@@ -8,33 +7,22 @@ const OPEXPER: f64 = 0.1;
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+
     let ui_handle = ui.as_weak();
-
-    ui.on_divide_income(move |string: SharedString| {
-        let ui: AppWindow = ui_handle.unwrap();
+    ui.on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
         let num: f64 = string.trim().parse().unwrap();
-
-        let tax: f64 = num * TAXPER;
+        let taxes: f64 = num * TAXPER;
         let owner: f64 = num * OWNERPER;
         let profit: f64 = num * PROFITPER;
         let opex: f64 = num * OPEXPER;
-
-        let result: String = format!(
+        let result = format!(
             "Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}",
-            {
-                tax
-            },
-            {
-                owner
-            },
-            {
-                profit
-            },
-            {
-                opex
-            }
+            taxes,
+            owner,
+            profit,
+            opex
         );
-
         ui.set_results(result.into());
     });
 
